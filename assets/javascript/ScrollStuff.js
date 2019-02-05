afed9ccd47d2ce4a9103ac08bba10012c6c0a50a
Milan Donhowe <milanaugust@gmail.com>
2019-02-05 07:43:56 -0800

    fixed animations?

--- assets/javascript/ScrollStuff.js
+++ assets/javascript/ScrollStuff.js
@@ -1,11 +1,13 @@
 // Add scrolling revealing stuff
 
-// make an array of all my elements on the page.
 
 //test that the code runs
 console.log("Running script...");
 
-ScrollReveal().reveal(document.querySelectorAll('h1'));
-ScrollReveal().reveal(document.querySelectorAll('h3'));
-ScrollReveal().reveal(document.querySelectorAll('p'));
-ScrollReveal().reveal(document.querySelectorAll('img'));
+ScrollReveal().reveal(document.querySelectorAll('*', {
+	distance: "50px",
+	duration: 700,
+	easing: 'ease-in',
+	origin: 'right',
+	reset:true
+}));
