2974747ed73e1916e2980faac81cde4c6451f25f
Milan Donhowe <milanaugust@gmail.com>
2019-02-05 07:53:58 -0800

    fix animations

--- assets/javascript/ScrollStuff.js
+++ assets/javascript/ScrollStuff.js
@@ -4,10 +4,19 @@
 //test that the code runs
 console.log("Running script...");
 
-ScrollReveal().reveal(document.querySelectorAll('*', {
+// the elements I want
+let headerOne = document.querySelectorAll('h1:not(.project-name)');
+let headerThree = document.querySelectorAll('h3');
+let imageTag = document.querySelectorAll('img');
+let pTag = document.querySelectorAll('p');
+
+let Elems = headerOne.concat(headerThree, imageTag, pTag);
+
+
+ScrollReveal().reveal(Elems, {
 	distance: "50px",
-	duration: 700,
+	duration: 600,
 	easing: 'ease-in',
 	origin: 'right',
-	reset:true
-}));
+	reset: true,
+});
